0dc50347c6acd4d8a33cdf92436619186684820e
raulbolsantos <raulbolsantos@gmail.com>
2017-02-10 23:27:20 -0200

    Mudança do domínio para usar SSL na comunicação com o servidor

--- app/src/main/java/projetocaronas/tcc/ifsp/br/projetocarona/utils/AndroidUtilsCaronas.java
+++ app/src/main/java/projetocaronas/tcc/ifsp/br/projetocarona/utils/AndroidUtilsCaronas.java
@@ -20,8 +20,7 @@ import java.util.List;
  * Created by Raul on 26/05/2016.
  */
 public class AndroidUtilsCaronas {
-    public final static String SERVER_PREFIX_CONTEXT = "http://ec2-35-164-212-117.us-west-2.compute.amazonaws.com:8080/caronas";
-//public final static String SERVER_PREFIX_CONTEXT = "https://jpldelvecchio.no-ip.org:8091";
+    public final static String SERVER_PREFIX_CONTEXT = "https://caronas-ifsp.tk:8080/caronas";
     // Armazena o prontuário usado para login no dispositivo
     public static String userRecord = "";
 
